7e00cbe467a3d6730f86797d87898f209b8c1c64
Nolan Byrnes <nolanehs@gmail.com>
2018-03-01 17:40:27 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -13,6 +13,18 @@ class RecipesController < ApplicationController
 
   def create
     recipe = Recipe.create(recipe_params)
-  redirect_to recipe_path
+  redirect_to recipe_path(recipe)
   end
+
+private
+
+  def recipe_params
+    params.require(:recipe).permit(
+      :title,
+      ingredients_attributes: [
+        :name,
+        :quantity,
+      ]
+    )
+  
 end
